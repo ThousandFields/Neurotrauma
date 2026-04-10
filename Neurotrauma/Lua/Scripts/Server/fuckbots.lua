f4e0600bb60a3d35d3eb15ed39fda53512e7240c
OlegBSTU <polybox45@gmail.com>
2026-04-11 00:29:15 +0500

    Add extensibility for npc affliction callouts

--- Neurotrauma/Lua/Scripts/Server/fuckbots.lua
+++ Neurotrauma/Lua/Scripts/Server/fuckbots.lua
@@ -24,6 +24,48 @@ if NTConfig.Get("NT_disableBotAlgorithms", true) then
 	end, Hook.HookMethodType.Before)
 end
 
+local afflictions = {
+	"n_fracture", -- urgent perceivable afflictions
+	"h_arterialcut",
+	"ll_arterialcut",
+	"rl_arterialcut",
+	"ra_arterialcut",
+	"la_arterialcut",
+	"sym_hematemesis", -- urgent causes
+	"sym_paleskin",
+	"sym_confusion",
+	"sym_lightheadedness",
+	"pain_abdominal",
+	"inflammation",
+	"gangrene",
+	"fever",
+	"sym_headache",
+	"sym_blurredvision",
+	"t_fracture", -- not urgent afflictions
+	"h_fracture",
+	"ra_fracture",
+	"la_fracture",
+	"rl_fracture",
+	"ll_fracture",
+	"dislocation1",
+	"dislocation2",
+	"dislocation3",
+	"dislocation4",
+	"pain_chest", -- not urgent causes
+	"sym_weakness",
+	"sym_sweating",
+	"dyspnea",
+	"sym_bloating",
+	"sym_legswelling",
+	"sym_craving",
+	"sym_palpitations",
+}
+NT.SymsForNPC = { ntaffs = afflictions }
+
+-- How to add own symptoms example:
+--local goobertable = { "goober", "gooberer" }
+--table.insert(NT.SymsForNPC, goobertable)
+
 -- allows npcs to talk about their neuro afflictions
 
 Hook.Patch("Barotrauma.HumanAIController", "SpeakAboutIssues", function(instance)
@@ -36,49 +78,23 @@ Hook.Patch("Barotrauma.HumanAIController", "SpeakAboutIssues", function(instance
 		chatType = ChatMessageType.Radio
 	end
 
-	local afflictions = {
-		"n_fracture", -- urgent perceivable afflictions
-		"h_arterialcut",
-		"ll_arterialcut",
-		"rl_arterialcut",
-		"ra_arterialcut",
-		"la_arterialcut",
-		"sym_hematemesis", -- urgent causes
-		"sym_paleskin",
-		"sym_confusion",
-		"sym_lightheadedness",
-		"pain_abdominal",
-		"inflammation",
-		"gangrene",
-		"fever",
-		"sym_headache",
-		"sym_blurredvision",
-		"t_fracture", -- not urgent afflictions
-		"h_fracture",
-		"ra_fracture",
-		"la_fracture",
-		"rl_fracture",
-		"ll_fracture",
-		"dislocation1",
-		"dislocation2",
-		"dislocation3",
-		"dislocation4",
-		"pain_chest", -- not urgent causes
-		"sym_weakness",
-		"sym_sweating",
-		"dyspnea",
-		"sym_bloating",
-		"sym_legswelling",
-		"sym_craving",
-		"sym_palpitations",
-	}
-
-	for identifier in afflictions do
+	for identifier in NT.SymsForNPC.ntaffs do
 		if HF.HasAffliction(character, identifier, 1) then
 			message = TextManager.Get("npcdialogsym." .. identifier)
 			print(message)
 			character.Speak(message, chatType, math.random(0, 5), Identifier(identifier .. "DialogSym"), 600.0)
 			break
+		end
+	end
+
+	for table in NT.SymsForNPC do
+		for identifier in table do
+			if HF.HasAffliction(character, identifier, 1) then
+				message = TextManager.Get("npcdialogsym." .. identifier)
+				print(message)
+				character.Speak(message, chatType, math.random(0, 5), Identifier(identifier .. "DialogSym"), 600.0)
+				break
+			end
 		end
 	end
 end, Hook.HookMethodType.After)
