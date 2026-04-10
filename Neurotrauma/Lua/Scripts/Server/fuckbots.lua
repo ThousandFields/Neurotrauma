7ca8a69d79060f80c7c52051d093266d38ab8205
OlegBSTU <78216412+OlegBSTU@users.noreply.github.com>
2026-04-10 15:06:05 +0300

    Dialogs for symptoms (#116)
    * Update fuckbots.lua
    
    * Add proper en-ru localisations and extra code
    
    * Formatting
    
    * Added some commenting
    
    ---------
    
    Co-authored-by: ThousandFields <mattthejayfeatherfan@gmail.com>

--- Neurotrauma/Lua/Scripts/Server/fuckbots.lua
+++ Neurotrauma/Lua/Scripts/Server/fuckbots.lua
@@ -1,3 +1,5 @@
+LuaUserData.MakeMethodAccessible(Descriptors["Barotrauma.HumanAIController"], "SpeakAboutIssues")
+
 -- hopefully this stops bots from doing any rescuing at all.
 -- and also hopefully my assumption that this very specific thing
 -- about bots is what is causing them to eat frames is correct.
@@ -21,3 +23,61 @@ if NTConfig.Get("NT_disableBotAlgorithms", true) then
 		return false
 	end, Hook.HookMethodType.Before)
 end
+
+-- allows npcs to talk about their neuro afflictions
+
+Hook.Patch("Barotrauma.HumanAIController", "SpeakAboutIssues", function(instance)
+	local character = instance.Character
+
+	local message = ""
+
+	local chatType = ChatMessageType.Default
+	if ChatMessage.CanUseRadio(character) then
+		chatType = ChatMessageType.Radio
+	end
+
+	local afflictions = {
+		"n_fracture", -- urgent perceivable afflictions
+		"h_arterialcut",
+		"ll_arterialcut",
+		"rl_arterialcut",
+		"ra_arterialcut",
+		"la_arterialcut",
+		"sym_hematemesis", -- urgent causes
+		"sym_paleskin",
+		"sym_confusion",
+		"sym_lightheadedness",
+		"pain_abdominal",
+		"inflammation",
+		"gangrene",
+		"fever",
+		"sym_headache",
+		"sym_blurredvision",
+		"t_fracture", -- not urgent afflictions
+		"h_fracture",
+		"ra_fracture",
+		"la_fracture",
+		"rl_fracture",
+		"ll_fracture",
+		"dislocation1",
+		"dislocation2",
+		"dislocation3",
+		"dislocation4",
+		"pain_chest", -- not urgent causes
+		"sym_weakness",
+		"sym_sweating",
+		"dyspnea",
+		"sym_bloating",
+		"sym_legswelling",
+		"sym_craving",
+		"sym_palpitations",
+	}
+
+	for identifier in afflictions do
+		if HF.HasAffliction(character, identifier, 1) then
+			message = TextManager.Get("npcdialogsym." .. identifier)
+			print(message)
+			character.Speak(message, chatType, math.random(0, 5), Identifier(identifier .. "DialogSym"), 600.0)
+		end
+	end
+end, Hook.HookMethodType.After)
